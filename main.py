6935aba9231bb8c0ac6b36b6f53a6114143e928c
Shashtttt <160567221+Shashtttt@users.noreply.github.com>
2025-06-15 20:56:10 +0530

    Update main.py

--- main.py
+++ main.py
@@ -248,4 +248,6 @@ if __name__ == '__main__':
     os.makedirs('templates', exist_ok=True)
     os.makedirs('static', exist_ok=True)
     init_db()
+    with app.app_context():
+        db.create_all()
     app.run(debug=True, host='0.0.0.0', port=5000)
