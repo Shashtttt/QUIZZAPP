2debad27ec9e1fb78b5dd90289b885c61b8fafe9
Shashtttt <160567221+Shashtttt@users.noreply.github.com>
2025-06-16 08:17:02 +0530

    Update main.py

--- main.py
+++ main.py
@@ -249,4 +249,5 @@ def quiz_list():
 if __name__ == '__main__':
     os.makedirs('templates', exist_ok=True)
     os.makedirs('static', exist_ok=True)
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(debug=True, host='0.0.0.0', port=port)
